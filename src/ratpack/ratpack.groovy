adf8ae6aa9001233c35234458c1b31f368192c7a
Luke Daley <ld@ldaley.com>
2013-07-22 12:44:03 -0500

    remove some accidentally committed stuff.

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,7 +1,6 @@
 import groovywebconsole.ReloadingThing
 import groovywebconsole.ScriptExecutor
 import org.ratpackframework.groovy.templating.TemplateRenderer
-import org.ratpackframework.groovy.templating.TemplatingModule
 
 import static groovy.json.JsonOutput.toJson
 import static org.ratpackframework.groovy.RatpackScript.ratpack
@@ -20,16 +19,6 @@ ratpack {
 
         get("reloadexample") {
             response.send new ReloadingThing().toString()
-        }
-
-        prefix(":a/:b") {
-            handler {
-                response.addHeader("foo", "bar")
-                next()
-            }
-            get(":c/:d?") {
-                response.send(pathTokens.toString())
-            }
         }
 
         assets "public"
